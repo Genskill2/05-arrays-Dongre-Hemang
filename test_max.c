6f5abcad23361b53f2c8dd569ff27b7e4cd05184
Dongre-Hemang <84710644+Dongre-Hemang@users.noreply.github.com>
2021-06-14 23:04:42 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -12,13 +12,3 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
-
-int max(int array[],int elements){
-  int max=array[0];
-  for(int i=0;i<elements;i++){
-    if(array[i]>max){
-      max=array[i];
-    }
-  }
-  return max;
-}
