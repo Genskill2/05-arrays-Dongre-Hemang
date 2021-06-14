bf8cd03c72db7cc0a02015b68931fcb688f7abb1
Dongre-Hemang <84710644+Dongre-Hemang@users.noreply.github.com>
2021-06-14 23:10:17 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -21,33 +21,5 @@ int main(void) {
   assert (ret[1] == 13);
   printf("Factors: passed\n");
 }
-int checkPrime(int n){
-       int checked=0;
-       for(int i=2;i<n;i++){
-           if(n%i==0){
-              return 0;
-           }
-           else{
-                return 1;
-           }
-        }
-   }
 
-int factors(int number,int ret[]){
-    int i=2,count=0;
-    
-        while(i<number){
-            while(number%i==0){
-                if(checkPrime(i)==1){
-                    number/=i;
-                    ret[count]=i;
-                    count++;
-                 }
-            } 
-            i++;
-        }
-        printf("%d\n",count);
-        return count;    
-}
-    
    
